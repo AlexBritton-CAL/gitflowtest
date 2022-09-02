182e880c2d51ad7b525bdca8970337bff4760ab5
Alex Britton <alex@ahbritton.com>
2022-09-02 17:35:12 +0100

    first commit

---
+++ config/prod_backend.tf
@@ -0,0 +1,5 @@
+use_msi              = true
+tenant_id            = "9c29d70b-9e7a-44d5-abe2-7e53df86502f"
+subscription_id      = "ff65e97e-c6be-4f35-b6b7-e52ce88bf5b2"
+storage_account_name = "terraformstateab2021"
+resource_group_name  = "GitflowTestPROD"
