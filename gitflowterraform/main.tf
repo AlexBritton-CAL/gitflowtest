cd60dc3730aca23ad4e3ca0f8363660f858c7256
Alex Britton <alex@ahbritton.com>
2022-09-04 12:50:38 +0100

    make code more genereic

--- gitflowterraform/main.tf
+++ gitflowterraform/main.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
 }
 
 locals {
-  config_content = file("../config/${var.config_name}.yaml")
+  config_content = file("../config/${var.config_name}.yml")
   config                = yamldecode(local.config_content)
 }
 
