03acd1c58bc98dcd4a6decba2109fc93057bf704
Alex Britton <alex@ahbritton.com>
2022-09-06 18:56:31 +0100

    test new vnet code

--- gitflowterraform/main.tf
+++ gitflowterraform/main.tf
@@ -27,13 +27,13 @@ resource "azurerm_private_dns_zone" "example" {
 
 resource "azurerm_network_security_group" "example" {
   name                = "example-security-group"
-  location            = azurerm_resource_group.example.location
+  location            = data.azurerm_resource_group.example.location
   resource_group_name = data.azurerm_resource_group.example.name
 }
 
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
-  location            = azurerm_resource_group.example.location
+  location            = data.azurerm_resource_group.example.location
   resource_group_name = data.azurerm_resource_group.example.name
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
