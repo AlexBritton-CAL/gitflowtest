c14fb804e31266c99994a38f7793c8defd1ee87b
Alex Britton <alex@ahbritton.com>
2022-09-07 12:00:06 +0100

    test new vnet code

--- gitflowterraform/main.tf
+++ gitflowterraform/main.tf
@@ -52,4 +52,5 @@ resource "azurerm_subnet" "example2" {
   resource_group_name  = data.azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
+  private_link_service_network_policies_enabled = false
 }
